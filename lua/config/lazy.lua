6b524b908444fb9d3038dcdf3d3129d00c3c7fb1
GarethSimmans <gareth.simmans@gmail.com>
2025-03-03 14:25:47 +0000

    add basic vim options

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,20 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.g.autoformat = true
+vim.g.lazyvim_picker = "auto"
+
+local opt = vim.opt
+
+opt.confirm = true 
+opt.cursorline = true
+opt.cursorline = true
+opt.expandtab = true
+opt.number = true
+opt.relativenumber = true
+opt.smartcase = true
+opt.smartindent = true
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
